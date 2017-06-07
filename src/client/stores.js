f455badad21c11626f6020afd13b3d209c7f2fa2
Layton Whiteley <layton.whiteley@gmail.com>
2017-06-07 00:06:36 -0500

    add comments

--- src/client/stores.js
+++ src/client/stores.js
@@ -1,5 +1,11 @@
 import home from './components/Home/HomeStore'
 
+/**
+ * stores exported here will be accessible in
+ * using the inject decorator with the name used
+ * in the export object
+ * for eg. @inject('home')
+ */
 export default {
     home
 };
