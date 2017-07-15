7425235cf2bda3e57bfab5cb8134915807ca88d8
Layton Whiteley <layton.whiteley@gmail.com>
2017-07-15 00:22:05 -0500

    format

--- src/client/client.js
+++ src/client/client.js
@@ -5,7 +5,11 @@ import { preload } from 'react-router-server';
 import App from './app';
 
 const domReady = (callback) => {
-  return (document.readyState === 'interactive' || document.readyState === 'complete') ? callback() : document.addEventListener('DOMContentLoaded', callback);
+  return (
+      document.readyState === 'interactive' ||
+      document.readyState === 'complete')
+      ? callback()
+      : document.addEventListener('DOMContentLoaded', callback);
 };
 
 domReady(() => {
